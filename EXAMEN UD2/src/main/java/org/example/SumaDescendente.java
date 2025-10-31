02b67086f2bc394edbfe087dcf6de06ba0c1e7ca
A702\dam1 <iriis.workflow@gmail.com>
2025-10-31 19:29:34 +0100

    SumaDescendente rectificada

--- EXAMEN UD2/src/main/java/org/example/SumaDescendente.java
+++ EXAMEN UD2/src/main/java/org/example/SumaDescendente.java
@@ -1,8 +1,23 @@
 package org.example;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class SumaDescendente {
 
     static void main() {
+
+        Scanner read = new Scanner(System.in);
+
+        int suma = 0
+
+        Random aleatorio = new Random();
+
+        for (int i = 500; i < 100000; i--)
+        System.out.println("Suma descendente del número generado" + aleatorio);
+        aleatorio /
+
+
 
     }
 
